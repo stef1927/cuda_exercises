4c0b6e85577b8c6ca2701cf4f723c0fbffb5bc8f
Stefania Alborghetti <salborghetti@nvidia.com>
2025-10-07 18:10:26 +0200

    Use unsigned ints to access global memory

--- src/histogram.cu
+++ src/histogram.cu
@@ -1,7 +1,8 @@
+#include <cassert>
+#include <cstdlib>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <random>
-#include <stdlib.h>
 
 #include "argparse.hpp"
 #include "cuda_utils.h"
@@ -92,8 +93,8 @@ bool verifyHistogram(unsigned int *histogramCPU, unsigned int *histogramGPU) {
 // with one block owning a private histogram and merging it into the global
 // histogram. The coarsening factor is the number of input elements that one
 // thread is responsible for updating in the private histogram.
-__global__ void histogramKernel(unsigned char *d_inputData,
-                                unsigned int *d_histogramGPU, int byte_count,
+__global__ void histogramKernel(const unsigned int *d_inputData, int input_size,
+                                unsigned int *d_histogramGPU,
                                 int coarsening_factor) {
 
   // Use a private histogram per block, allocate it in shared memory to be of
@@ -109,8 +110,12 @@ __global__ void histogramKernel(unsigned char *d_inputData,
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   for (int i = idx * coarsening_factor; i < (idx + 1) * coarsening_factor;
        ++i) {
-    if (i < byte_count) {
-      atomicAdd(&s_histogram[d_inputData[i]], 1);
+    if (i < input_size) {
+      unsigned int data = d_inputData[i];
+      atomicAdd(&s_histogram[(data >> 0) & 0xFFU], 1);
+      atomicAdd(&s_histogram[(data >> 8) & 0xFFU], 1);
+      atomicAdd(&s_histogram[(data >> 16) & 0xFFU], 1);
+      atomicAdd(&s_histogram[(data >> 24) & 0xFFU], 1);
     }
   }
   __syncthreads();
@@ -125,14 +130,24 @@ __global__ void histogramKernel(unsigned char *d_inputData,
 
 void launchKernel(unsigned char *d_inputData, unsigned int *d_histogramGPU,
                   const Args &args, cudaStream_t stream) {
+
+  // The kernel will load input data as unsigned ints to reduce the number of
+  // uncoalesced accesses from the global memory.
+  if (args.byte_count % sizeof(unsigned int) != 0) {
+    throw std::runtime_error("Byte count must be divisible by " +
+                             std::to_string(sizeof(unsigned int)));
+  }
+
   dim3 dimBlock(args.block_size);
   // The number of items processed by one block is the size of the block times
   // the coarsening factor because each thread is responsible for coarsening
   // factor number of items.
   int dimBlockTotal = dimBlock.x * args.coarsening_factor;
   dim3 dimGrid((args.byte_count + dimBlockTotal - 1) / dimBlockTotal);
-  histogramKernel<<<dimGrid, dimBlock, 0, stream>>>(
-      d_inputData, d_histogramGPU, args.byte_count, args.coarsening_factor);
+  int input_size = args.byte_count / sizeof(unsigned int);
+  histogramKernel<<<dimGrid, dimBlock, 0, stream>>>((unsigned int *)d_inputData,
+                                                    input_size, d_histogramGPU,
+                                                    args.coarsening_factor);
   cudaCheck(cudaGetLastError());
 }
 
