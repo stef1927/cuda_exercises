af6f28bd97a035a01c80377478b7cb982802d5ca
Stefania Alborghetti <salborghetti@nvidia.com>
2025-11-02 14:22:20 -0500

    Make matrices bigger, remove CPU multiplication from gpu file

--- src/matrix_mul_gpu.cu
+++ src/matrix_mul_gpu.cu
@@ -39,19 +39,19 @@ int parse_args(int argc, char* argv[], Args& args, cudaDeviceProp& deviceProp) {
   program.add_argument("--width")
       .help("width of the matrix")
       .scan<'i', int>()
-      .default_value(512)
+      .default_value(4096)
       .store_into(args.width);
 
   program.add_argument("--height")
       .help("height of the matrix")
       .scan<'i', int>()
-      .default_value(512)
+      .default_value(4096)
       .store_into(args.height);
 
   program.add_argument("--block-size")
       .help("block size")
       .scan<'i', int>()
-      .default_value(32)
+      .default_value(16)
       .store_into(args.block_size);
 
   try {
@@ -91,21 +91,6 @@ int parse_args(int argc, char* argv[], Args& args, cudaDeviceProp& deviceProp) {
   return 0;
 }
 
-
-template <Numeric T, typename LayoutA = RowMajor, typename LayoutB = RowMajor, typename LayoutC = RowMajor>
-void matrixMulCpu(const Matrix<T, HostMemoryAllocator, LayoutA>& A, const Matrix<T, HostMemoryAllocator, LayoutB>& B,
-                  Matrix<T, HostMemoryAllocator, LayoutC>& C) {
-  Timer timer("matrix multiplication on CPU");
-  for (int row = 0; row < A.height(); row++) {
-    for (int col = 0; col < B.width(); col++) {
-      T sum = 0.0;
-      for (int k = 0; k < A.width(); k++) {
-        sum += A(row, k) * B(k, col);
-      }
-      C(row, col) = sum;
-    }
-  }
-}
 
 template <Numeric T, typename LayoutA = RowMajor, typename LayoutB = RowMajor, typename LayoutC = RowMajor>
 __global__ void matrixMulNaiveKernel(Matrix<T, NoAllocator, LayoutA> A, Matrix<T, NoAllocator, LayoutB> B,
@@ -158,7 +143,7 @@ __global__ void matrixMulTiledKernel(Matrix<T, NoAllocator, LayoutA> A, Matrix<T
 template <Numeric T, typename LayoutA = RowMajor, typename LayoutB = RowMajor, typename LayoutC = RowMajor>
 void run_kernel(KernelType kernelType, Matrix<T, DeviceAsyncMemoryAllocator, LayoutA>& dA,
                 Matrix<T, DeviceAsyncMemoryAllocator, LayoutB>& dB, Matrix<T, DeviceAsyncMemoryAllocator, LayoutC>& dC,
-                CudaStream& streamWrapper, int block_size) {
+                Matrix<T, HostMemoryAllocator, LayoutC>& C, CudaStream& streamWrapper, int block_size) {
   cudaStream_t stream = streamWrapper.stream;
   CudaEventRecorder recorder = streamWrapper.record("matrix multiplication on GPU");
 
@@ -186,20 +171,10 @@ void run_kernel(KernelType kernelType, Matrix<T, DeviceAsyncMemoryAllocator, Lay
       " WorkgroupSize= %u threads/block\n",
       dB.layout.name, kernel_type_to_string(kernelType).c_str(), tFlops, matrixMulTimeMsec, flopsPerMatrixMul,
       block_size * block_size);
-}
 
-
-// Copy data from device to host and verify results
-template <Numeric T, typename LayoutC = RowMajor>
-void verify_results(Matrix<T, HostMemoryAllocator, LayoutC>& C, Matrix<T, DeviceAsyncMemoryAllocator, LayoutC>& dC,
-                    const Matrix<T, HostMemoryAllocator, LayoutC>& C_ref, cudaStream_t stream) {
   cudaCheck(cudaMemcpyAsync(C.data, dC.data, C.size() * sizeof(T), cudaMemcpyDeviceToHost, stream));
   cudaCheck(cudaStreamSynchronize(stream));
-
-  printf("Verifying results\n");
-  C.verify(C_ref);
 }
-
 
 int main(int argc, char* argv[]) {
   cudaDeviceProp deviceProp = getDeviceProperties(0, true);
@@ -210,16 +185,12 @@ int main(int argc, char* argv[]) {
 
   Matrix<bf16, HostMemoryAllocator> A(args.width, args.height);
   Matrix<bf16, HostMemoryAllocator> B(args.width, args.height);
-  Matrix<bf16, HostMemoryAllocator> C(args.width, args.height);
-  Matrix<bf16, HostMemoryAllocator> C_ref(args.width, args.height);
+  Matrix<bf16, HostMemoryAllocator> C1(args.width, args.height);
+  Matrix<bf16, HostMemoryAllocator> C2(args.width, args.height);
 
   A.randomize(generator);
   B.randomize(generator);
 
-  printf("Performaing matrix multiplication on CPU\n");
-  matrixMulCpu<bf16>(A, B, C_ref);
-
-  printf("Initializing device matrices\n");
   CudaStream streamWrapper;
   cudaStream_t stream = streamWrapper.stream;
 
@@ -230,9 +201,11 @@ int main(int argc, char* argv[]) {
   cudaCheck(cudaMemcpyAsync(dA.data, A.data, A.size() * sizeof(bf16), cudaMemcpyHostToDevice, stream));
   cudaCheck(cudaMemcpyAsync(dB.data, B.data, B.size() * sizeof(bf16), cudaMemcpyHostToDevice, stream));
 
-  for (auto kernel_type : {KernelType::NAIVE, KernelType::TILED}) {
-    run_kernel<bf16>(kernel_type, dA, dB, dC, streamWrapper, args.block_size);
-    verify_results(C, dC, C_ref, stream);
-  }
+  // Run naive and tiled kernels, assuming naive is correct, verify the results match
+  run_kernel<bf16>(KernelType::NAIVE, dA, dB, dC, C1, streamWrapper, args.block_size);
+  run_kernel<bf16>(KernelType::TILED, dA, dB, dC, C2, streamWrapper, args.block_size);
+
+  C1.verify(C2);
+
   return 0;
 }
