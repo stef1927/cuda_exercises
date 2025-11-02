8abd52a626271542a74a58ee47000b2b0bc54962
Stefania Alborghetti <salborghetti@nvidia.com>
2025-11-01 21:40:43 -0400

    Fix mem coalescing by using float2 and __half2

--- src/coordinates.cu
+++ src/coordinates.cu
@@ -17,17 +17,10 @@ struct Args {
   int block_size;
 };
 
-template <Numeric T>
-struct PolarCoord {
-  T r;      // radius
-  T theta;  // angle in radians
-};
-
-template <Numeric T>
-struct CartesianCoord {
-  T x;
-  T y;
-};
+using CartesianCoordFloat = float2;
+using CartesianCoordHalf = __half2;
+using PolarCoordFloat = float2;  // r in .x, theta in .y
+using PolarCoordHalf = __half2;  // r in .x, theta in .y
 
 enum class KernelType {
   NAIVE,
@@ -61,43 +54,43 @@ int parse_args(int argc, char* argv[], Args& args) {
 }
 
 
-std::vector<CartesianCoord<float>> generate_cartesian_coordinates(int N) {
+std::vector<CartesianCoordFloat> generate_cartesian_coordinates(int N) {
   Timer timer("generate_cartesian_coordinates");
-  std::vector<CartesianCoord<float>> cartesian_coordinates;
+  std::vector<CartesianCoordFloat> cartesian_coordinates;
   cartesian_coordinates.reserve(N);
   std::default_random_engine generator(786);
   std::uniform_real_distribution<float> distribution(-100.0f, 100.0f);
   for (int i = 0; i < N; i++) {
     float x = distribution(generator);
     float y = distribution(generator);
-    cartesian_coordinates.push_back(CartesianCoord<float>{x, y});
+    cartesian_coordinates.push_back(make_float2(x, y));
   }
   return cartesian_coordinates;
 }
 
-std::vector<PolarCoord<float>> cartesian_to_polar_cpu(const std::vector<CartesianCoord<float>>& cartesian_coordinates) {
+std::vector<PolarCoordFloat> cartesian_to_polar_cpu(const std::vector<CartesianCoordFloat>& cartesian_coordinates) {
   Timer timer("cartesian_to_polar_cpu");
-  std::vector<PolarCoord<float>> polar_coordinates(cartesian_coordinates.size(), PolarCoord<float>{0.0f, 0.0f});
+  std::vector<PolarCoordFloat> polar_coordinates(cartesian_coordinates.size(), make_float2(0.0f, 0.0f));
   std::transform(cartesian_coordinates.begin(), cartesian_coordinates.end(), polar_coordinates.begin(),
-                 [](const CartesianCoord<float>& cartesian_coordinate) -> PolarCoord<float> {
+                 [](const CartesianCoordFloat& cartesian_coordinate) -> PolarCoordFloat {
                    float r = sqrt(cartesian_coordinate.x * cartesian_coordinate.x +
                                   cartesian_coordinate.y * cartesian_coordinate.y);
                    float theta = atan2(cartesian_coordinate.y, cartesian_coordinate.x);
-                   return PolarCoord{r, theta};
+                   return make_float2(r, theta);
                  });
   return polar_coordinates;
 }
 
 
-bool verify_results(const std::vector<PolarCoord<float>>& polar_coordinates,
-                    const std::vector<PolarCoord<float>>& polar_coordinates_ref, double tolerance = 1e-4) {
+bool verify_results(const std::vector<PolarCoordFloat>& polar_coordinates,
+                    const std::vector<PolarCoordFloat>& polar_coordinates_ref, double tolerance = 1e-4) {
   auto ret =
       std::ranges::equal(polar_coordinates, polar_coordinates_ref,
-                         [tolerance](const PolarCoord<float>& a, const PolarCoord<float>& b) -> bool {
-                           auto ret = std::fabs(a.r - b.r) < tolerance && std::fabs(a.theta - b.theta) < tolerance;
+                         [tolerance](const PolarCoordFloat& a, const PolarCoordFloat& b) -> bool {
+                           auto ret = std::fabs(a.x - b.x) < tolerance && std::fabs(a.y - b.y) < tolerance;
                            if (!ret) {
                              printf("Divergence! r: %5.4f != %5.4f (Diff %5.4f), theta: %5.4f != %5.4f (Diff %5.4f)",
-                                    b.r, a.r, std::fabs(a.r - b.r), b.theta, a.theta, std::fabs(a.theta - b.theta));
+                                    b.x, a.x, std::fabs(a.x - b.x), b.y, a.y, std::fabs(a.y - b.y));
                            }
                            return ret;
                          });
@@ -106,42 +99,41 @@ bool verify_results(const std::vector<PolarCoord<float>>& polar_coordinates,
 }
 
 
-__global__ void cartesian_to_polar_kernel_naive(CartesianCoord<float>* _cartesian_coordinates,
-                                                PolarCoord<float>* d_polar_coordinates, int N) {
+__global__ void cartesian_to_polar_kernel_naive(CartesianCoordFloat* _cartesian_coordinates,
+                                                PolarCoordFloat* d_polar_coordinates, int N) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < N) {
-    CartesianCoord<float> coord = _cartesian_coordinates[idx];
+    CartesianCoordFloat coord = _cartesian_coordinates[idx];
     float r = sqrt(coord.x * coord.x + coord.y * coord.y);
     float theta = atan2(coord.y, coord.x);
-    PolarCoord<float> polar_coord = PolarCoord<float>{r, theta};
+    PolarCoordFloat polar_coord = make_float2(r, theta);
     d_polar_coordinates[idx] = polar_coord;
   }
 }
 
 
-__global__ void cartesian_to_polar_kernel_optimized_full(CartesianCoord<float>* __restrict__ _cartesian_coordinates,
-                                                         PolarCoord<float>* __restrict__ _polar_coordinates, int N) {
+__global__ void cartesian_to_polar_kernel_optimized_full(CartesianCoordFloat* __restrict__ _cartesian_coordinates,
+                                                         PolarCoordFloat* __restrict__ _polar_coordinates, int N) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < N) {
-    CartesianCoord<float> coord = _cartesian_coordinates[idx];
+    CartesianCoordFloat coord = _cartesian_coordinates[idx];
     float r = hypotf(coord.x, coord.y);
     float theta = atan2f(coord.y, coord.x);
-    PolarCoord<float> polar_coord = PolarCoord<float>{r, theta};
+    PolarCoordFloat polar_coord = make_float2(r, theta);
     _polar_coordinates[idx] = polar_coord;
   }
 }
 
-void run_kernel_full_precision(std::vector<CartesianCoord<float>>& cartesian_coordinates,
-                               std::vector<PolarCoord<float>>& polar_coordinates, int block_size,
-                               KernelType kernelType) {
+void run_kernel_full_precision(std::vector<CartesianCoordFloat>& cartesian_coordinates,
+                               std::vector<PolarCoordFloat>& polar_coordinates, int block_size, KernelType kernelType) {
   Timer timer("cartesian_to_polar_kernel");
   auto N = cartesian_coordinates.size();
   auto streamWrapper = CudaStream();
   cudaStream_t stream = streamWrapper.stream;
-  auto d_cartesian_coordinates = make_cuda_unique<CartesianCoord<float>>(N);
+  auto d_cartesian_coordinates = make_cuda_unique<CartesianCoordFloat>(N);
   cudaCheck(cudaMemcpyAsync(d_cartesian_coordinates.get(), cartesian_coordinates.data(),
-                            N * sizeof(CartesianCoord<float>), cudaMemcpyHostToDevice, streamWrapper.stream));
-  auto d_polar_coordinates = make_cuda_unique<PolarCoord<float>>(N);
+                            N * sizeof(CartesianCoordFloat), cudaMemcpyHostToDevice, streamWrapper.stream));
+  auto d_polar_coordinates = make_cuda_unique<PolarCoordFloat>(N);
 
 
   auto recorder = streamWrapper.record("cartesian_to_polar_kernel");
@@ -166,40 +158,39 @@ void run_kernel_full_precision(std::vector<CartesianCoord<float>>& cartesian_coo
   cudaCheck(cudaGetLastError());
   recorder.close();
 
-  cudaCheck(cudaMemcpyAsync(polar_coordinates.data(), d_polar_coordinates.get(), N * sizeof(PolarCoord<float>),
+  cudaCheck(cudaMemcpyAsync(polar_coordinates.data(), d_polar_coordinates.get(), N * sizeof(PolarCoordFloat),
                             cudaMemcpyDeviceToHost, streamWrapper.stream));
   cudaCheck(cudaStreamSynchronize(streamWrapper.stream));
 }
 
-__global__ void cartesian_to_polar_kernel_optimized_half(CartesianCoord<__half>* __restrict__ _cartesian_coordinates,
-                                                         PolarCoord<__half>* __restrict__ _polar_coordinates, int N) {
+__global__ void cartesian_to_polar_kernel_optimized_half(CartesianCoordHalf* __restrict__ _cartesian_coordinates,
+                                                         PolarCoordHalf* __restrict__ _polar_coordinates, int N) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < N) {
-    CartesianCoord<__half> coord = _cartesian_coordinates[idx];
+    CartesianCoordHalf coord = _cartesian_coordinates[idx];
     __half r = hypotf(coord.x, coord.y);
     __half theta = atan2f(coord.y, coord.x);
-    PolarCoord<__half> polar_coord = PolarCoord<__half>{r, theta};
+    PolarCoordHalf polar_coord = make_half2(r, theta);
     _polar_coordinates[idx] = polar_coord;
   }
 }
 
 
-void run_kernel_half_precision(std::vector<CartesianCoord<float>>& cartesian_coordinates,
-                               std::vector<PolarCoord<float>>& polar_coordinates, int block_size) {
+void run_kernel_half_precision(std::vector<CartesianCoordFloat>& cartesian_coordinates,
+                               std::vector<PolarCoordFloat>& polar_coordinates, int block_size) {
   Timer timer("cartesian_to_polar_kernel");
   auto N = cartesian_coordinates.size();
   auto streamWrapper = CudaStream();
   cudaStream_t stream = streamWrapper.stream;
-  auto cartesian_coordinates_half = std::vector<CartesianCoord<__half>>(N, CartesianCoord<__half>{0.0f, 0.0f});
+  auto cartesian_coordinates_half = std::vector<CartesianCoordHalf>(N, make_half2(0.0f, 0.0f));
   std::transform(cartesian_coordinates.begin(), cartesian_coordinates.end(), cartesian_coordinates_half.begin(),
-                 [](const CartesianCoord<float>& cartesian_coordinate) -> CartesianCoord<__half> {
-                   return CartesianCoord<__half>{__float2half(cartesian_coordinate.x),
-                                                 __float2half(cartesian_coordinate.y)};
+                 [](const CartesianCoordFloat& cartesian_coordinate) -> CartesianCoordHalf {
+                   return make_half2(__float2half(cartesian_coordinate.x), __float2half(cartesian_coordinate.y));
                  });
-  auto d_cartesian_coordinates = make_cuda_unique<CartesianCoord<__half>>(N);
-  cudaCheck(cudaMemcpyAsync(d_cartesian_coordinates.get(), cartesian_coordinates.data(),
-                            N * sizeof(CartesianCoord<__half>), cudaMemcpyHostToDevice, streamWrapper.stream));
-  auto d_polar_coordinates = make_cuda_unique<PolarCoord<__half>>(N);
+  auto d_cartesian_coordinates = make_cuda_unique<CartesianCoordHalf>(N);
+  cudaCheck(cudaMemcpyAsync(d_cartesian_coordinates.get(), cartesian_coordinates.data(), N * sizeof(CartesianCoordHalf),
+                            cudaMemcpyHostToDevice, streamWrapper.stream));
+  auto d_polar_coordinates = make_cuda_unique<PolarCoordHalf>(N);
 
   auto recorder = streamWrapper.record("cartesian_to_polar_kernel");
   dim3 dimBlock(block_size);
@@ -211,13 +202,13 @@ void run_kernel_half_precision(std::vector<CartesianCoord<float>>& cartesian_coo
   cudaCheck(cudaGetLastError());
   recorder.close();
 
-  auto polar_coordinates_half = std::vector<PolarCoord<__half>>(N, PolarCoord<__half>{0.0f, 0.0f});
-  cudaCheck(cudaMemcpyAsync(polar_coordinates_half.data(), d_polar_coordinates.get(), N * sizeof(PolarCoord<__half>),
+  auto polar_coordinates_half = std::vector<PolarCoordHalf>(N, make_half2(0.0f, 0.0f));
+  cudaCheck(cudaMemcpyAsync(polar_coordinates_half.data(), d_polar_coordinates.get(), N * sizeof(PolarCoordHalf),
                             cudaMemcpyDeviceToHost, streamWrapper.stream));
   cudaCheck(cudaStreamSynchronize(streamWrapper.stream));
   std::transform(polar_coordinates_half.begin(), polar_coordinates_half.end(), polar_coordinates.begin(),
-                 [](const PolarCoord<__half>& polar_coordinate) -> PolarCoord<float> {
-                   return PolarCoord<float>{__half2float(polar_coordinate.r), __half2float(polar_coordinate.theta)};
+                 [](const PolarCoordHalf& polar_coordinate) -> PolarCoordFloat {
+                   return make_float2(__half2float(polar_coordinate.x), __half2float(polar_coordinate.y));
                  });
 }
 
@@ -231,7 +222,7 @@ int main(int argc, char* argv[]) {
 
   auto cartesian_coordinates = generate_cartesian_coordinates(args.N);
   auto polar_coordinates_ref = cartesian_to_polar_cpu(cartesian_coordinates);
-  auto polar_coordinates = std::vector<PolarCoord<float>>(args.N, PolarCoord<float>{0.0f, 0.0f});
+  auto polar_coordinates = std::vector<PolarCoordFloat>(args.N, make_float2(0.0f, 0.0f));
 
   run_kernel_full_precision(cartesian_coordinates, polar_coordinates, args.block_size, KernelType::NAIVE);
   ret |= verify_results(polar_coordinates, polar_coordinates_ref);
